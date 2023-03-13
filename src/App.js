c56c244c08eafa57d89133ed34cf0ca356539006
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-03-14 00:36:40 +0530

    static-expenses-commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     { title: "Car Wash", date: new Date(2023, 3, 14), amount: 22.5 },
     { title: "Dog Food", date: new Date(2023, 3, 13), amount: 2.2 },
     { title: "Shopping", date: new Date(2023, 3, 12), amount: 622.9 },
+    { title: "Electricity Bill", date: new Date(2023, 3, 12), amount: 242.9 }
   ];
-
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>Hi, I am Deepesh Bhardwaj</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
+      <h2>Hi, I'm Deepesh Bhardwaj!</h2>
+      <p>This is an Expense Tracker Web Application</p>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
